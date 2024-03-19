3ef3c66b04c673a85628af8b549250ba7d1d8a93
gginnn <ginnyliu@ocadu.ca>
2024-03-19 00:29:17 -0400

    blah

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
 document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('#image-gallery img');
-    const quoteModal = document.getElementById('quote-modal');
-    const quoteText = document.getElementById('quote');
-    const secondQuoteText = document.getElementById('second-quote');
-    const closeBtn = document.querySelector('.close');
+
+    function showModal(modal) {
+        modal.style.display = "block";
+    }
+
+    function hideModal(modal) {
+        modal.style.display = "none";
+    }
 
     images.forEach(image => {
         image.addEventListener('click', function() {
+            const modalClass = this.getAttribute('data-modal-class');
+            const modal = document.querySelector(`#quote-modal.${modalClass}`);
             const quote = this.getAttribute('data-quote');
             const secondQuote = this.getAttribute('data-second-quote');
 
-            quoteText.textContent = quote;
-            secondQuoteText.textContent = secondQuote;
-
-            quoteModal.style.display = 'block';
+            updateQuote(modal, quote, secondQuote);
+            showModal(modal);
         });
     });
 
-    closeBtn.addEventListener('click', function() {
-        quoteModal.style.display = 'none';
+    function updateQuote(modal, quote, secondQuote) {
+        const quoteText = modal.querySelector('#quote');
+        const secondQuoteText = modal.querySelector('#second-quote');
+        quoteText.textContent = quote;
+        secondQuoteText.textContent = secondQuote;
+    }
+
+    const closeBtns = document.querySelectorAll('.close');
+    closeBtns.forEach(closeBtn => {
+        closeBtn.addEventListener('click', function() {
+            const modal = this.closest('.modal');
+            hideModal(modal);
+        });
     });
 
-    quoteModal.addEventListener('click', function(event) {
-        if (event.target === quoteModal) {
-            quoteModal.style.display = 'none';
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        modal.addEventListener('click', function(event) {
+            if (event.target === modal) {
+                hideModal(modal);
+            }
+        });
+    });
+
+    const playButton = document.getElementById('playButton');
+    playButton.addEventListener('click', function() {
+        var audio = document.getElementById('myAudio');
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
         }
     });
 });
-
-
-
-
